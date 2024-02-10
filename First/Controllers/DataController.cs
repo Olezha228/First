51112dbec4925db1aa96ff290aff3b54fe9e88a9
Олег <@DESKTOP-D64AONM>
2024-02-10 13:37:34 +0500

    Add project files.

---
+++ First/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new { Name = "John", Age = 30 };
+            return Ok(data);
+        }
+    }
+}
